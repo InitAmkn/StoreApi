9ac668e2dedb6db3fe1a312df4f281e2ef8d4b1e
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2024-01-22 22:45:43 +0300

    Добавьте файлы проекта.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,87 @@
+﻿
+using WebApi.Abstraction;
+using WebApi.Models.DTO;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+        private readonly IProductRepository _productRepository;
+
+
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet("get_products")]
+        public IActionResult GetProducts()
+        {
+            var products = _productRepository.GetProducts();
+            return Ok(products);
+        }
+
+        [HttpGet("get_groups")]
+        public IActionResult GetGroups()
+        {
+            var groups = _productRepository.GetGroups();
+            return Ok(groups);
+        }
+
+        [HttpPost("add_product")]
+        public IActionResult AddProduct([FromBody] ProbuctDto productDto)
+        {
+            var result = _productRepository.AddProduct(productDto);
+            return Ok(result);
+        }
+
+        [HttpPost("add_group")]
+        public IActionResult AddGroup([FromBody] GroupDto groupDto)
+        {
+            var result = _productRepository.AddGroup(groupDto);
+            return Ok(result);
+        }
+        //ДЗ-2
+        [HttpGet("export_products_csv")]
+        public IActionResult ExportProductsCsv()
+        {
+            var products = _productRepository.GetProducts();
+
+            using (var memoryStream = new MemoryStream())
+            using (var writer = new StreamWriter(memoryStream))
+            using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                csv.WriteRecords(products);
+                writer.Flush();
+                memoryStream.Position = 0;
+
+                return File(memoryStream, "text/csv", "products.csv");
+            }
+        }
+        //ДЗ-2
+        [HttpGet("cache_statistics_csv")]
+        public IActionResult GetCacheStatisticsCsv()
+        {
+            var cacheStatistics = _productRepository.GetCacheStatistics();
+
+            var memoryStream = new MemoryStream();
+            using (var writer = new StreamWriter(memoryStream))
+            using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                csv.WriteRecord(cacheStatistics);
+                writer.Flush();
+                memoryStream.Position = 0;
+
+                return File(memoryStream, "text/csv", "cache_statistics.csv");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi.Models
+{
+    public class Product : BaseModel
+    {
+        public string Description { get; set; } = null!;
+        public int GroupID { get; set; }
+        public virtual Group Group { get; set; } = null!;
+        public int Price { get; set; }
+        public virtual List<Store> Stores { get; set; } = null!;
+        public int GroupId { get; set; }
+
+    }
+}
